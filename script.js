9d3ad42d57299a576bdf090da5d9d9d8d08148ee
Putumani <putumaninomzaza@gmail.com>
2024-08-04 01:54:42 +0200

    applying DRY code principle by adding helper functions

--- script.js
+++ script.js
@@ -6,106 +6,93 @@ document.addEventListener("DOMContentLoaded", function () {
   );
   let allPagesSelected = false;
 
+  function applyVariants(element, variantsToAdd, variantsToRemove) {
+    variantsToAdd.forEach((variant) => element.classList.add(variant));
+    variantsToRemove.forEach((variant) => element.classList.remove(variant));
+  }
+
+  function handleMouseEnter(component) {
+    if (allPagesSelected) {
+      if (component.classList.contains("variant5")) {
+        applyVariants(component, ["variant5"], ["variant4"]);
+      } else {
+        applyVariants(component, ["variant5"], ["variant1"]);
+      }
+    } else {
+      if (component.classList.contains("variant5")) {
+        applyVariants(component, ["variant4"], ["variant5"]);
+      } else if (!component.classList.contains("variant4")) {
+        applyVariants(component, ["variant2"], ["variant1"]);
+      }
+    }
+  }
+
+  function handleMouseLeave(component) {
+    if (allPagesSelected) {
+      if (component.classList.contains("variant5")) {
+        applyVariants(component, ["variant5"], ["variant4"]);
+      }
+    } else {
+      if (component.classList.contains("variant4")) {
+        applyVariants(component, ["variant5"], ["variant4"]);
+      } else {
+        applyVariants(component, ["variant1"], ["variant2", "variant3"]);
+      }
+    }
+  }
+
+  function handleClick(component) {
+    if (component.classList.contains("variant4")) {
+      applyVariants(component, ["variant5-transition"], ["variant4"]);
+      setTimeout(() => {
+        applyVariants(component, ["variant2"], ["variant5-transition"]);
+      }, 300);
+    } else {
+      applyVariants(component, ["variant4"], ["variant1", "variant5"]);
+    }
+  }
+
   function updateComponentState() {
     if (allPagesSelected) {
-      clickableComponents.forEach((comp) => {
-        comp.classList.add("variant5");
-        comp.classList.remove("variant2");
-        comp.classList.remove("variant1");
-      });
-      allPagesClickableComponent.classList.add("variant4");
-      allPagesClickableComponent.classList.remove("variant5");
+      clickableComponents.forEach((comp) =>
+        applyVariants(comp, ["variant5"], ["variant2", "variant1"])
+      );
+      applyVariants(allPagesClickableComponent, ["variant4"], ["variant5"]);
     } else {
-      clickableComponents.forEach((comp) => {
-        comp.classList.remove("variant4");
-        comp.classList.remove("variant5");
-        comp.classList.add("variant1");
-      });
-      allPagesClickableComponent.classList.remove("variant4");
-      allPagesClickableComponent.classList.remove("variant5");
+      clickableComponents.forEach((comp) =>
+        applyVariants(comp, ["variant1"], ["variant4", "variant5"])
+      );
+      applyVariants(allPagesClickableComponent, [], ["variant4", "variant5"]);
     }
   }
 
   clickableComponents.forEach((component) => {
-    component.addEventListener("mouseenter", function () {
-      if (allPagesSelected) {
-        if (component.classList.contains("variant5")) {
-          component.classList.add("variant5");
-          component.classList.remove("variant4");
-        } else {
-          component.classList.add("variant5");
-          component.classList.remove("variant1");
-        }
-      } else {
-        if (component.classList.contains("variant5")) {
-          component.classList.add("variant4");
-          component.classList.remove("variant5");
-        } else if (!component.classList.contains("variant4")) {
-          component.classList.add("variant2");
-          component.classList.remove("variant1");
-        }
-      }
-    });
-
-    component.addEventListener("mouseleave", function () {
-      if (allPagesSelected) {
-        if (component.classList.contains("variant5")) {
-          component.classList.add("variant5");
-          component.classList.remove("variant4");
-        }
-      } else {
-        if (component.classList.contains("variant4")) {
-          component.classList.add("variant5");
-          component.classList.remove("variant4");
-        } else {
-          component.classList.remove("variant2");
-          component.classList.add("variant1");
-          component.classList.remove("variant3");
-        }
-      }
-    });
-
-    component.addEventListener("mousedown", function () {
+    component.addEventListener("mouseenter", () => handleMouseEnter(component));
+    component.addEventListener("mouseleave", () => handleMouseLeave(component));
+    component.addEventListener("mousedown", () => {
       if (!component.classList.contains("variant4")) {
-        component.classList.add("variant3");
+        applyVariants(component, ["variant3"], []);
       }
     });
-
-    component.addEventListener("mouseup", function () {
+    component.addEventListener("mouseup", () => {
       if (!component.classList.contains("variant4")) {
-        component.classList.remove("variant3");
+        applyVariants(component, [], ["variant3"]);
       }
     });
-
-    component.addEventListener("click", function () {
-      if (component.classList.contains("variant4")) {
-        component.classList.add("variant5-transition");
-        component.classList.remove("variant4");
-        setTimeout(() => {
-          component.classList.remove("variant5-transition");
-          component.classList.add("variant2");
-        }, 300);
-      } else {
-        component.classList.add("variant4");
-        component.classList.remove("variant1");
-        component.classList.remove("variant5");
-      }
-    });
+    component.addEventListener("click", () => handleClick(component));
   });
 
-  allPagesClickableComponent.addEventListener("click", function () {
+  allPagesClickableComponent.addEventListener("click", () => {
     allPagesSelected = !allPagesSelected;
     updateComponentState();
   });
 
-  allPagesComponent.addEventListener("mouseleave", function () {
+  allPagesComponent.addEventListener("mouseleave", () => {
     if (allPagesSelected) {
-      allPagesClickableComponent.classList.add("variant5");
-      allPagesClickableComponent.classList.remove("variant4");
-      clickableComponents.forEach((comp) => {
-        comp.classList.add("variant5");
-        comp.classList.remove("variant4");
-      });
+      applyVariants(allPagesClickableComponent, ["variant5"], ["variant4"]);
+      clickableComponents.forEach((comp) =>
+        applyVariants(comp, ["variant5"], ["variant4"])
+      );
       allPagesSelected = false;
     }
   });
